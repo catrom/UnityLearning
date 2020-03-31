ab36eb3914269485cf20c0d266bfe39bd1f03d51
catrom <thanhluanqn1998@gmail.com>
2020-03-31 20:03:15 +0700

    .

---
+++ Source/New Unity Project/Assets/move.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(0, 5, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody>().AddForce(0, 0, 3);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            GetComponent<Rigidbody>().AddForce(0, 0, -5);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody>().AddForce(-5, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody>().AddForce(5, 0, 0);
+        }
+    }
+}
